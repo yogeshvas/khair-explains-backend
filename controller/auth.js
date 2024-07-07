3d2f8159a7297676ed2cc479868699052634664f
Yogesh Vashisth <writetokhair@gmail.com>
2024-07-08 00:58:44 +0530

    flow changed

--- controller/auth.js
+++ controller/auth.js
@@ -1,6 +1,7 @@
 import bcrypt from "bcrypt";
 import { generateTokenAndSetCookie } from "../lib/utils/generateTokenAndSetCookie.js";
 import { User } from "../model/user-model.js";
+import jwt from "jsonwebtoken";
 export const signup = async (req, res) => {
   try {
     const { name, email, password } = req.body;
@@ -57,11 +58,15 @@ export const login = async (req, res) => {
     if (!user || !isPasswordCorrect) {
       return res.status(400).json({ error: "Invalid username or password" });
     }
-    generateTokenAndSetCookie(user._id, res);
+
+    const payload = { userId: user._id };
+    const token = jwt.sign(payload, process.env.JWT_SECRET);
     res.status(200).json({
       _id: user._id,
       name: user.name,
+      avatar: user.avatar,
       email: user.email,
+      token,
     });
   } catch (error) {
     console.log("error in login controller" + error.message);
@@ -73,10 +78,30 @@ export const login = async (req, res) => {
 
 export const getMe = async (req, res) => {
   try {
-    const user = await User.findById(req.user._id).select("-password");
+    // Get token from header
+    const token = req.header("Authorization").replace("Bearer ", "");
+    if (!token) {
+      return res.status(401).json({ error: "No token provided" });
+    }
+
+    // Verify token
+    const decoded = jwt.verify(token, process.env.JWT_SECRET);
+    if (!decoded) {
+      return res.status(401).json({ error: "Invalid token" });
+    }
+
+    // Find user by ID
+    const user = await User.findById(decoded.userId).select("-password");
+    if (!user) {
+      return res.status(404).json({ error: "User not found" });
+    }
+
+    // Send user data
     res.status(200).json(user);
   } catch (error) {
-    console.log("Error in getMe controller", error.message);
-    res.status(500).json({ error: error.message });
+    console.log("Error in getMe controller:", error.message);
+    res
+      .status(500)
+      .json({ error: "Internal Server Error", message: error.message });
   }
 };
--- route/auth.js
+++ route/auth.js
@@ -3,7 +3,7 @@ import { getMe, login, signup } from "../controller/auth.js";
 import { protectRoute } from "../middlewares/ProtectRoute.js";
 
 const router = express.Router();
-router.route("/me").get(protectRoute, getMe);
+router.route("/me").get(getMe);
 router.route("/signup").post(signup);
 router.route("/login").post(login);
 
